2dd6910b584a977408ad3b73cdd437fc69f266a1
BryanSetuke <156883514+BryanSetuke@users.noreply.github.com>
2024-06-11 15:50:59 +0200

    COMMIT

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,24 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
 
 module.exports = {
-  // entry: './public/index.html',
-  entry: './src/index.ts',
-  output: {
-    filename: 'index.pack.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+    entry: {
+        index: './src/index.ts'
     },
-    compress: true,
-    port: 8082,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public/images', to: 'images' },
-        { from: 'public/index.css', to: 'index.css' }  // Ensure CSS file is copied to the dist directory
-      ],
-    }),
-  ],
-};
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].pack.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devtool: 'source-map'
+}
